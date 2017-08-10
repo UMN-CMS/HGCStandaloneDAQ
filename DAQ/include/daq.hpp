a03ca09a9681e8929cc95a75f6c5a99c8efa4051
Ryan Quinn <quinn.ryanx@gmail.com>
2017-08-10 21:36:53 +0000

    Added xml files in the etc/ directory. Added header file for the daq as well.

---
+++ DAQ/include/daq.hpp
@@ -0,0 +1,9 @@
+#ifndef DAQ_HPP
+#define DAQ_HPP
+
+#include "uhal/uhal.hpp"
+
+// do stuff
+const int DATASIZE = 30784;     // number of words to read
+
+#endif
--- DAQ/src/common/daq.cxx
+++ DAQ/src/common/daq.cxx
@@ -1,12 +1,28 @@
 #include "uhal/uhal.hpp"
 
+#include <vector>
+#include <string>
 #include <iostream>
 
 
 
 int main(int argc, char *argv[]) {
 
-    std::cout << "Test!\n";
+    /*** initialization ***/
+    uhal::setLogLevelTo(uhal::Error());// otherwise there's a bunch of crap printed out
+
+    // iterators
+    int i, j, k;
+    unsigned int ui, uj, uk;
+
+    std::vector<uhal::HwInterface> rdouts;
+
+
+    /*** connection set-up ***/
+    uhal::ConnectionManager manager("etc/connections.xml");
+    std::vector<std::string> ids = manager.getDevices(".*rdout.*");// get all rdout boards
+    for(ui = 0; ui < ids.size(); ui++)
+        rdouts.push_back(manager.getDevice(ids[ui]));
 
     return 0;
 }
