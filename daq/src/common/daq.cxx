33811e8474f4f298cbaeb0e81b9d1152f5676532
Ryan Quinn <quinn.ryanx@gmail.com>
2017-08-11 21:09:58 +0000

    Updated Makefile to use C++11. Using it in daq.cxx too

--- daq/src/common/daq.cxx
+++ daq/src/common/daq.cxx
@@ -21,8 +21,19 @@ int main(int argc, char *argv[]) {
     /*** connection set-up ***/
     uhal::ConnectionManager manager("etc/connections.xml");
     std::vector<std::string> ids = manager.getDevices(".*rdout.*");// get all rdout boards
-    for(ui = 0; ui < ids.size(); ui++)
-        rdouts.push_back(manager.getDevice(ids[ui]));
+    for(auto id : ids)
+        rdouts.push_back(manager.getDevice(id));
+
+    // get connected cables (fifos)
+    // loop over events
+    // wait for block ready
+    // read events
+    // check fifo empty, if not then read til empty
+    // send rdout_done
+    // end event loop
+    // closing actions
+    // maybe use multiple threads?
+    // edit makefile for C++11?
 
     return 0;
 }
