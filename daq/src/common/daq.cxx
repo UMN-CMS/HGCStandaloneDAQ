730b0e6c57da4851fcc9560b68af4cd6f7ad479f
Ryan Quinn <quinn.ryanx@gmail.com>
2017-08-12 00:35:23 +0000

    Updated DAQ to wait check if the fifo is empty and keep reading until it is

--- daq/src/common/daq.cxx
+++ daq/src/common/daq.cxx
@@ -60,7 +60,9 @@ int main(int argc, char *argv[]) {
             // get the event data
             std::vector<uint32_t> ev_data = get_nwords(rdout, FIFO, BLOCKSIZE);
 
-            // TODO check fifo empty, if not then read til empty
+            // check if the fifo is empty, if not then read til it is empty
+            while(!get_word(rdout, EMPTY))
+                (void) get_word(rdout, FIFO);
 
             // send rdout done magic
             put_word(rdout, RDOUT_DONE, RDOUT_DONE_MAGIC);
